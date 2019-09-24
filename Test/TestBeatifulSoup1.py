b114b12ecf9fa1b5efa4f2f35d98018089357fcb
aaronyinyong <aaronyinyong@hotmail.com>
2019-09-24 20:52:13 +0800

    First

--- Test/TestBeatifulSoup1.py
+++ Test/TestBeatifulSoup1.py
@@ -2,18 +2,24 @@
 
 from bs4 import BeautifulSoup
 import requests
+import socket
 
-headers1={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:55.0) Gecko/20100101 Firefox/55.0"}
-r = requests.get('https://www.lixinger.com/analytics/company/sz/300383/detail/announcement?announcement-type=fs&page-index=0',timeout=10,headers=headers1).text
+socket.setdefaulttimeout(10)
 
-source=requests.get('https://www.lixinger.com/analytics/company/sz/300383/detail/announcement?announcement-type=fs&page-index=0').text
+# headers1={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:55.0) Gecko/20100101 Firefox/55.0"}
+send_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",    "Connection": "keep-alive",    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",    "Accept-Language": "zh-CN,zh;q=0.8"    }
+
+r = requests.get('https://www.lixinger.com/analytics/company/sz/300383/detail/announcement?announcement-type=fs&page-index=0',timeout=10,headers=send_headers)
+r.close()
+print(r.text)
+# source=requests.get('https://www.lixinger.com/analytics/company/sz/300383/detail/announcement?announcement-type=fs&page-index=0').text
 
 # source=requests.get('http://vip.stock.finance.sina.com.cn/corp/go.php/vCB_Bulletin/stockid/002271/page_type/ndbg.phtml').text
 
 
-soup=BeautifulSoup(r,'lxml')
+soup=BeautifulSoup(r.text,'lxml')
 
-print(soup)
+# print(soup)
 
 
 
--- Test/Test_getweblink.py
+++ Test/Test_getweblink.py
@@ -9,7 +9,10 @@ import requests
 from bs4 import BeautifulSoup
 
 headers1={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:55.0) Gecko/20100101 Firefox/55.0"}
-r = requests.get('https://www.lixinger.com/analytics/company/sz/300383/detail/announcement?announcement-type=fs&page-index=0',timeout=10,headers=headers1)
+send_headers = {    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",    "Connection": "keep-alive",    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",    "Accept-Language": "zh-CN,zh;q=0.8"    }
+
+r = requests.get('https://www.lixinger.com/analytics/company/sz/300383/detail/announcement?announcement-type=fs&page-index=0',timeout=10,headers=send_headers)
+r.close()
 
 print(r.encoding)
 
